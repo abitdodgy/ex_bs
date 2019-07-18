e588c6b70ca2227e40a6be139558f2904f456326
Mohamad <husseini.mel@gmail.com>
2019-07-18 19:52:16 -0300

    Adds documentation to badges

--- lib/ex_bs/badge.ex
+++ lib/ex_bs/badge.ex
@@ -8,11 +8,44 @@ defmodule ExBs.Badge do
   @badge_types ExBs.Config.bootstrap(:badge_types)
 
   Enum.each(@badge_types, fn {type, _class} ->
+    @doc """
+    Returns a #{type} badge component. Accepts a list of
+    attributes that is passed onto the html tag.
+
+    Use the `shape: :pill` option to create a `badge-pill` component.
+
+    ## Examples
+
+        #{type}("Badge!")
+        #=> <div class="badge badge-#{type}">Badge!</div>
+
+        #{type}("Badge!", shape: :pill)
+        #=> <div class="badge badge-#{type} badge-pill">Badge!</div>
+
+    """
     def unquote(type)(text, opts \\ []) do
       badge(unquote(type), text, opts)
     end
   end)
 
+
+  @doc """
+  Returns a badge component. Accepts a list of attributes
+  that is passed onto the html tag.
+
+  Pass an atom as the first argument to set variation.
+
+  Use the `shape: :pill` option to create a `badge-pill` component.
+
+  ## Examples
+
+      badge(:success)
+      #=> <div class="badge badge-success">Badge!</div>
+
+      badge(:success, "Badge!", shape: :pill)
+      #=> <div class="badge badge-success badge-pill">Badge!</div>
+
+  """
   def badge(type, text, opts \\ []) do
     {_, opts} =
       opts
